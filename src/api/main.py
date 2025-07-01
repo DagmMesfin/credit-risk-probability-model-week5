93d7e088086c429bf1e70c9df063cbff9a963757
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2025-07-01 21:22:51 +0300

    fix: the probability fixed to risk probability

--- src/api/main.py
+++ src/api/main.py
@@ -18,11 +18,11 @@ def predict_risk(input: FeaturesRequest):
     try:
         X = np.array(list(input.features.values())).reshape(1, -1)
         prediction = model.predict(X)
-        probability = model.predict_proba(X)[:, 1]  # Get the probability of the positive class
+        probability = model.predict_proba(X)[0]  # Get the probability of the risk class
         prediction = prediction[0]  # Extract single prediction
         return {
             "is_credible": float(prediction),
-            "probability": float(probability[0])
+            "risk_probability": float(probability[0])
         }
     except Exception as e:
         return {"error": str(e)}
